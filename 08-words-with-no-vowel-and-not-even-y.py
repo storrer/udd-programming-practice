9e1669828d260cccd878c1eb9deee38560060d60
storrer <storrer.chris@gmail.com>
2023-07-30 05:55:40 -0700

    08 completed.

--- 08-words-with-no-vowel-and-not-even-y.py
+++ 08-words-with-no-vowel-and-not-even-y.py
@@ -1,2 +1,19 @@
 # What are all of the words with no vowel and not even a Y?
 input_filename = "sowpods.txt"
+
+def find_words_without_vowels(filename):
+    words_without_vowels_even_Y = []
+    vowels = "AEIOUY"
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    for word in words:
+        for vowel in vowels:
+            if vowel in word:
+                break
+        else:
+            words_without_vowels_even_Y.append(word.strip())
+    
+    return words_without_vowels_even_Y
+
+for word in find_words_without_vowels(input_filename):
+    print(word)
