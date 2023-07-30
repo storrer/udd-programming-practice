cc1fcf8b6285935f12256a2635fed6c58fc19a81
storrer <storrer.chris@gmail.com>
2023-07-30 07:39:34 -0700

    13 is completed.

--- 13-shortest-word-with-all-5-vowels.py
+++ 13-shortest-word-with-all-5-vowels.py
@@ -1,6 +1,7 @@
 # What is the shortest word that contains all 5 vowels?
 input_filename = "sowpods.txt"
 
+# Using a function I wrote for problem 09:
 def find_words_with_each_vowels(filename):
     words_with_each_vowel = []
     vowels = "AEIOU"
@@ -21,5 +22,27 @@ def find_words_with_each_vowels(filename):
     
     return words_with_each_vowel
 
-for word in find_words_with_each_vowels(input_filename):
-    print(word)
+# There may be a tie for shortest, so let's return a list
+def find_shortest_words(words_list): 
+    # Assume the first word is the shortest   
+    shortest_length = len(words_list[0])
+    shortest_words = [words_list[0]]
+    
+    # Begin looping with 
+    for word in words_list[1:]:
+        word_length = len(word)
+        # If we find a shorter word, re-assign out result array to that word
+        if word_length < shortest_length:
+            shortest_length = word_length
+            shortest_words = [word] 
+        elif word_length == shortest_length:
+            shortest_words.append(word)
+    
+    return shortest_words
+
+
+words_to_compare = find_words_with_each_vowels(input_filename)
+shortest_words = find_shortest_words(words_to_compare)
+
+print("The shortest word(s) with all five vowels: ", shortest_words)
+print("The length of the shortest word(s): ", len(shortest_words[0]), " letters")
