88a6c1901089d292ff0d0ab36fc8f44e22a5f6e3
storrer <storrer.chris@gmail.com>
2023-07-30 04:48:22 -0700

    03 solved.

--- 03-words-containing-q-but-not-u.py
+++ 03-words-containing-q-but-not-u.py
@@ -1,2 +1,16 @@
 # What are all of the words containing a Q but not a U?
- 
+input_filename = "sowpods.txt"
+
+def find_words_with_Q_not_U(filename):
+    words_with_Q_not_U = []
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    
+    for word in words:
+        if word.count("Q") and not word.count("U"):
+            words_with_Q_not_U.append(word.strip())
+    
+    return words_with_Q_not_U
+
+for word in find_words_with_Q_not_U(input_filename):
+    print(word)
