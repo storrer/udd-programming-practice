2ac6f5a1a0c56912fd19317be71b2619a62ab5fc
storrer <storrer.chris@gmail.com>
2023-07-30 05:04:06 -0700

    04 and 05 solved.

--- 04-words-containing-cat-and-5-letters.py
+++ 04-words-containing-cat-and-5-letters.py
@@ -1,1 +1,16 @@
 # What are all of the words that contain the word CAT and are exactly 5 letters long?
+input_filename = "sowpods.txt"
+
+def find_five_letter_words_with_CAT(filename):
+    five_letter_words_with_CAT = []
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    
+    for word in words:
+        if word.count("CAT") and len(word.strip()) == 5:
+            five_letter_words_with_CAT.append(word.strip())
+    
+    return five_letter_words_with_CAT
+
+for word in find_five_letter_words_with_CAT(input_filename):
+    print(word)
--- 05-words-with-no-e-or-a-and-15-letters.py
+++ 05-words-with-no-e-or-a-and-15-letters.py
@@ -1,1 +1,18 @@
 # What are all of the words that have no E or A and are at least 15 letters long?
+
+input_filename = "sowpods.txt"
+
+def find_long_words_without_E_or_A(filename):
+    long_words_without_E_or_A = []
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    
+    for word in words:
+        # I read that word.count() is faster but it sure takes a lot of typing
+        if (word.count("E") == 0) and (word.count("A") == 0) and len(word.strip()) > 14:
+            long_words_without_E_or_A.append(word.strip()) 
+    
+    return long_words_without_E_or_A
+
+for word in find_long_words_without_E_or_A(input_filename):
+    print(word)
