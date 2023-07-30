3113635d158aa5f8d2baf44b99da66da2c507f9b
storrer <storrer.chris@gmail.com>
2023-07-30 05:31:36 -0700

    06 and 07 solved.

--- 07-words-starting-and-ending-with-y.py
+++ 07-words-starting-and-ending-with-y.py
@@ -1,3 +1,18 @@
 # What are all of the words that both start and end with a Y?
 
 input_filename = "sowpods.txt"
+
+def find_words_starting_and_ending_with_Y(filename):
+    words_starting_and_ending_with_Y = []
+    with open(filename, 'r') as file:
+        words = file.readlines()
+    
+    for word in words:
+        # Access start and end of the words with [0] and [-2]
+        if word[0] == 'Y' and word[-2] == 'Y':
+            words_starting_and_ending_with_Y.append(word.strip()) 
+    
+    return words_starting_and_ending_with_Y
+
+for word in find_words_starting_and_ending_with_Y(input_filename):
+    print(word)
