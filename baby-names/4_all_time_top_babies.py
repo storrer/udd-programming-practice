cf76d730765838e104c4bb0da9a8949e8fb30eca
storrer <storrer.chris@gmail.com>
2023-09-04 18:24:59 -0700

    Add last baby names question.

--- baby-names/4_all_time_top_babies.py
+++ baby-names/4_all_time_top_babies.py
@@ -1,1 +1,35 @@
 # What are all of the names that were top 40 baby names in both 1880 and 2020?
+
+# Read in 1880
+with open('E:\\Projects\\udd-programming-practice\\baby-names\\baby_names_1880_short.txt', 'r') as file:
+    # create a set from names   
+    names_1880 = set(file.readlines())
+
+if names_1880:
+    print("Pass")
+else:
+    print(names_1880)
+
+# read in 2020
+with open('E:\\Projects\\udd-programming-practice\\baby-names\\baby_names_2020_short.txt', 'r') as file_two:
+    # create a set from names
+    names_2020 = set(file_two.readlines())
+
+empty_list = []
+assert not empty_list
+# Assumption of length 40
+assert len(names_2020) == 40
+
+#TODO come up with a better variable
+names_in_both_eras = []
+
+# compare sets for names appearing in both
+# 1. for loop to append names appearing in both to a result list
+for name in names_1880:
+    if name in names_2020:
+        names_in_both_eras.append(name)
+
+# 2. Use a union to retrieve the members present in both sets
+
+# Print names appearing in both sets
+print(*names_in_both_eras)
--- neetcode/contains_duplicate.py
+++ neetcode/contains_duplicate.py
@@ -4,9 +4,9 @@ Given an integer array nums, return true if any value appears at least twice in 
 Input: nums = [1,2,3,1]
 Output: true
 """
-"""
-Original implementation.
-def contains_duplicate(numbers):
+
+# Original implementation.
+"""def contains_duplicate1(numbers):
     nums_set = set(numbers) # (1,2,3) | (1,2,3,4) |(-1)
     for num in nums_set: # O(n)
         numbers.remove(num) # O(n) 
@@ -16,14 +16,14 @@ def contains_duplicate(numbers):
     for num in nums_set: # O(n)
         if num in numbers: # O(n)
             return True
-        
-    return False
-"""
-def contains_duplicate(numbers):
+    return False"""
+
+def contains_duplicate1(numbers):
     """
     Rather than pre-generate a set of all unique entries in numbers[], I am now
             
-    """ 
+    """
+    
     seen_nums = set()
     for num in numbers:
         if num in seen_nums: 
@@ -36,3 +36,15 @@ nums_two = [1,2,3,4] # False
 nums_three = [1,1,1,3,3,4,3,2,4,2]
 nums_four = [-1]
 nums_five = [-1,2,1,-2,-100,100] # False
+
+def containsDuplicate1(nums):
+  return len(nums) != len(list(set(nums)))
+
+def containsDuplicate2(nums):
+  return len(nums) != len(set(nums))
+
+import timeit
+"""
+print(timeit.timeit(containsDuplicate1, number=1000))
+print(timeit.timeit(containsDuplicate2, number=1000))
+"""
