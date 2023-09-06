42c62f9ea793e2e0f87a17076ff59006a220482b
storrer <storrer.chris@gmail.com>
2023-09-06 05:11:00 -0700

    Start problem 22 in wordplay

--- wordplay/22_free_wheel_of_fortune_words.py
+++ wordplay/22_free_wheel_of_fortune_words.py
@@ -2,3 +2,15 @@
 What are all of the words that can be made from only the letters in “RSTLNE”?
 Not all of those letters need to be used, and letters can be repeated.
 """
+
+# Function validating words
+def word_matches(word: str):
+    return len(word) == 11 and word.startswith("PRO") and word.endswith("ING")
+
+# Read in the file line by line/ word by word
+with open('sowpods.txt', 'r') as file:
+    for word in file:
+        # strip white space from the right side
+        word = word.rstrip()
+        if word_matches(word):
+            print(word) 
