c35e500a607edf348c9ee52120d59408bf23a7be
storrer <storrer.chris@gmail.com>
2023-09-20 15:36:33 -0700

    Add solution to two_sum_II_array_is_sorted

--- neetcode/two_sum_II_array_is_sorted.py
+++ neetcode/two_sum_II_array_is_sorted.py
@@ -27,7 +27,7 @@ Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We ret
 
 Constraints:
 
-- 2 <= numbers.length <= 3 * 104
+- 2 <= numbers.length <= 3 * 10^4
 - -1000 <= numbers[i] <= 1000
 - numbers is sorted in non-decreasing order.
 - -1000 <= target <= 1000
@@ -38,18 +38,51 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]|None:
+        #1. brute force: iterate over the array for each element
+        #2. set(numbers) - if target - current_element in set(numbers) ! no index given
+        #3. Two pointer:
+        left = 0
+        right = len(numbers) - 1
         
-        return [0,0]
+        while numbers[left] + numbers[right] != target:
+            sum = numbers[left] + numbers[right]   
+            if sum > target:
+                right -= 1
+            elif sum < target:
+                left += 1
+            if left == right:
+                return None
+        
+
+        return [left+1, right+1]
     
 
 
 # Tests
+sol = Solution()
+
 numbers1 = [2,7,11,15]
 target1 = 9
+print("Actual: ",sol.twoSum(numbers1,target1), " Expected [1,2]")
+
+numbers11 = [1,2,3]
+target11 = 12
+print("Actual: ",sol.twoSum(numbers11,target11), " Expected None")
+
+numbers12 = [1,2,3,4]
+target12 = 12
+print("Actual: ",sol.twoSum(numbers12,target12), " Expected None")
+
+
+
 
 numbers2 = [2,3,4]
 target2 = 6
+print("Actual: ",sol.twoSum(numbers2,target2), " Expected [1,3]")
 
 numbers3 = [-1,0]
 target3 = -1
+print("Actual: ",sol.twoSum(numbers3,target3), " Expected [1,2]")
+
+ 
