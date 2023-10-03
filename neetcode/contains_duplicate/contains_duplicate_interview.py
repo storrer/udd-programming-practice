5116f7db908fc338ffb834f718b295656b008005
storrer <storrer.chris@gmail.com>
2023-10-02 18:37:45 -0700

    Add valid_parentheses and solution

--- neetcode/contains_duplicate/contains_duplicate_interview.py
+++ neetcode/contains_duplicate/contains_duplicate_interview.py
@@ -19,23 +19,23 @@ class Solution:
 
 		return False
 
-
-def runTests():
-	sol = Solution()
-	test_empty_list = ([], False)
-	test_true1 = ([1,2,3,3], True)
-	test_false1 = ([1,2,3,4,10], False) 
- 
-	test_passed = True
-	test_inputs = [test_empty_list, test_true1, test_false1]
-	for input in test_inputs:
-		test_result = sol.containsDuplicate(input[0])
-		if test_result != input[1]:
-			print(f"Input: {input[0]} Expected: {input[1]} Actual: {test_result}")
-			test_passed = False
-
+if __name__ == "__main__":
+	def runTests():
+		sol = Solution()
+		test_empty_list = ([], False)
+		test_true1 = ([1,2,3,3], True)
+		test_false1 = ([1,2,3,4,10], False) 
 	
-	if test_passed:
-		print("All test passed.")
+		test_passed = True
+		test_inputs = [test_empty_list, test_true1, test_false1]
+		for input in test_inputs:
+			test_result = sol.containsDuplicate(input[0])
+			if test_result != input[1]:
+				print(f"Input: {input[0]} Expected: {input[1]} Actual: {test_result}")
+				test_passed = False
 
-runTests()
+		
+		if test_passed:
+			print("All test passed.")
+
+	runTests()
---
+++ neetcode/contains_duplicate/test_containts_duplicate_interview.py
@@ -0,0 +1,5 @@
+import pytest
+from contains_duplicate_interview import Solution
+
+class TestSolution():
+    pass
