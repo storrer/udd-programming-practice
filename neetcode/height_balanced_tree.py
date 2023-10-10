2e37d948ff6e7db35711c4868f54ddfb42ea4345
storrer <storrer.chris@gmail.com>
2023-10-10 14:18:13 -0700

    Add balanced tree sol'n

--- neetcode/height_balanced_tree.py
+++ neetcode/height_balanced_tree.py
@@ -15,19 +15,37 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
+    depths = {}
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root == None:
             return 0
- 
-            
-        return 1 + max([self.maxDepth(root.left),self.maxDepth(root.right)])
+        
+        if root in self.depths:
+            return self.depths[root]
+
+        self.depths[root] = 1 + max([self.maxDepth(root.left),self.maxDepth(root.right)])
+        return self.depths[root]
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        if abs(self.maxDepth(root.left) -  self.maxDepth(root.right)) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        
+        return False
 
 # Inputs
 root1 = TreeNode(val=1,right=TreeNode(val=2))
-root2 = TreeNode(val=1,left=TreeNode(val=9),right=TreeNode(val=20, left=TreeNode(15),right=TreeNode(7)))
 #[3,9,20,null,null,15,7]
+root2 = TreeNode(val=3,left=TreeNode(val=9),right=TreeNode(val=20, left=TreeNode(15),right=TreeNode(7)))
+# Example 2
+root3 = TreeNode(val=1, left= TreeNode(val=2,left=TreeNode(val=3,left=TreeNode(val=4), right=TreeNode(val=4)),right=TreeNode(val=3)),right=TreeNode(val=2))
 sol = Solution()
-print(sol.maxDepth(root1))
-print(sol.maxDepth(root2))
+print(sol.isBalanced(root1))
+print(sol.isBalanced(root2))
+print(sol.isBalanced(root3))
 
+
+#MEMOIZE
