98cd6d54228bd5fb8db551c75cedfb585a7c829b
storrer <storrer.chris@gmail.com>
2023-10-20 13:43:27 -0700

    Add solution for top k frequent words

---
+++ neetcode/top_k_frequent.py
@@ -0,0 +1,58 @@
+"""
+Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+"""
+
+from typing import List
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # Create count dictionary empty
+        word_dict = {}
+        # Count occurrences in word list
+        dictionary = {"the": 1}
+        
+        # Sort k frequent words
+        for word in words:
+            if word in word_dict:
+                word_dict[word] += 1
+            else:
+                word_dict[word] = 1
+        
+        print(word_dict.items())
+
+        # Cast to list
+        freq_list = list(word_dict.items())
+        print("Frequency list", freq_list)
+
+        # Sort lexicographically
+        freq_list.sort(key=lambda x: x[0])
+
+        # Sort by count
+        freq_list.sort(key=lambda x: x[1], reverse=True)
+        print("Sorted frequencies", freq_list)
+
+        result = []
+
+        for element in freq_list[:k]:
+            result.append(element[0])
+        
+        return result
+
+
+
+
+words = ["i","love","leetcode","i","love","coding"]
+k = 1
+counts = {
+    "i": 2,
+    "love": 2,
+    "leetcode": 1,
+    "coding": 1
+}
+# If there is no tie, output is max(values) -> key
+counts_list = [("key",1), ("key2",2)]
+counts_list.sort(key=lambda x: x[1])
+#print(counts_list)
+
+
+sol = Solution()
+print(sol.topKFrequent(words, 2))
