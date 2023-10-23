61f20bc0c0d20897282314ac882f80cef37d84c4
storrer <storrer.chris@gmail.com>
2023-10-23 16:51:33 -0700

    File organization

--- neetcode/top_k_frequent/top_k_frequent.py
+++ neetcode/top_k_frequent/top_k_frequent.py
@@ -15,7 +15,7 @@ class Solution:
             if word in word_dict:
                 word_dict[word] += 1
             else:
-                word_dict[word] = 1
+                word_dict[word] = 1 
         
         print(word_dict.items())
 
