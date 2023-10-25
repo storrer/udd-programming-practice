2bebf75e949fb1ed0d0820332a8962e612307845
storrer <storrer.chris@gmail.com>
2023-10-25 13:19:21 -0700

    Add number_of_consistent_strings

---
+++ neetcode/number_of_consistent_strings_v1.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    # Go, C++ Style
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        # Cast string into a set (faster containment lookups) , maybe later if allowed is larger for some reason
+        
+        count = 0 # mutable
+        for word in words:
+            if all(char in allowed for char in word): # generator
+                count += 1
+        
+        return count
+    
+    # Ruby, Rust, Lisp
+    def countConsistentFunctional(self, allowed, words):
+        # no Mutables
+        # Produce iterator/generator
+        consistent = filter(lambda word: all(char in allowed for char in word), words)
+
+        return len(list(consistent))
+
+
+allowed = "ab"
+words = ["ad","bd","aaab","baa","badab"]
+
+"""
+
+nb: Functional programming avoids mutables.
+Learn functional style! #TODO
+"""
